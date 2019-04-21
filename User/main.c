a4fc5d537324764e103b13f8431813becb48e5b4
DuYihao <b17090713@njupt.edu.cn>
2019-04-21 22:24:07 +0800

    Test some basic function.

--- User/main.c
+++ User/main.c
@@ -5,30 +5,21 @@
 #include "driverlib/pin_map.h"
 #include "inc/hw_memmap.h"
 #include "driverlib/sysctl.h"
+#include "inc/hw_pwm.h"
 
-
-void PF_123_set_to_out(void);
+#include "delay.h"
+#include "pwm_config.h"
+#include "uart_config.h"
+#include "CarCtrl/car_ctrl.h"
 
 int main()
 {
-    PF_123_set_to_out();
+    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |  SYSCTL_XTAL_16MHZ);//50MHz
+
+    UART0_init(115200);
+    car_ctrl_init();
+    
     while(1)
     {
     }
-}
-void PF_123_set_to_out(void)
-{
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD);
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD);
-    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD);
-    
-    GPIODirModeSet(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_DIR_MODE_OUT);
-    GPIODirModeSet(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_DIR_MODE_OUT);
-    GPIODirModeSet(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_DIR_MODE_OUT);
-    
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
 }
