4d8f8a32a5842bf650575f639689d3db50a81d5e
DuYihao <b17090713@njupt.edu.cn>
2019-04-24 20:28:30 +0800

    Test tracking function.

--- User/main.c
+++ User/main.c
@@ -7,7 +7,6 @@
 #include "driverlib/sysctl.h"
 #include "inc/hw_pwm.h"
 
-#include <stdlib.h>
 #include "delay.h"
 #include "pwm_config.h"
 #include "uart_config.h"
@@ -18,13 +17,16 @@
 #include "display.h"
 #include "wifi.h"
 #include "qr_code.h"
+#include "timer_config.h"
 
+uint8_t track_addr;
+uint8_t forward[8],back[8],left[8],right[8];
 uint16_t servo_angle[] = {90, 90, 90, 90};
 
 int main()
 {
     SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);//50MHz
-    
+
     UART0_init(115200);
     car_ctrl_init();
     servo_ctrl_init(servo_angle);
@@ -33,11 +35,27 @@ int main()
     wifi_link_init(9600);
     camera_link_init(9600);
     qr_code_init(9600);
+
+    timer0_init(100);
     
+    UARTprintf("All Ready\n");
+    tracking_select(2);
     while(1)
     {
+        for(uint8_t i = 0; i < 8; i++)
+        {
+            UARTprintf("%d",back[i]);
+        }
+        UARTprintf("\n");
+        delay_ms(20);
     }
 }
+
+
+
+
+
+
 
 
 
