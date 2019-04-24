74d7dea1c2d9fbd5038a23d155f044b602e63290
DuYihao <b17090713@njupt.edu.cn>
2019-04-24 09:31:36 +0800

    Add all uart init code.

--- UART/uart_config.h
+++ UART/uart_config.h
@@ -4,7 +4,7 @@
  *  Created on: 2019.4.21
  *      Author: DuYihao
  */
-
+ 
 #ifndef _UART_CONFIG_H_
 #define _UART_CONFIG_H_
 
@@ -23,22 +23,22 @@
 
 void UART0_init(uint32_t Baud);
 
-//void UART1_Init(uint32_t Baud);
-//void IntHandler_UART1();
+void UART1_Init(uint32_t Baud);
+void IntHandler_UART1(void);
 
-//void UART2_Init(uint32_t Baud);
-//void IntHandler_UART2();
+void UART2_Init(uint32_t Baud);
+void IntHandler_UART2(void);
 
-//void UART3_Init(uint32_t Baud);
-//void IntHandler_UART3();
+void UART3_Init(uint32_t Baud);
+void IntHandler_UART3(void);
 
-//void UART4_Init(uint32_t Baud);
-//void IntHandler_UART4();
+void UART4_Init(uint32_t Baud);
+void IntHandler_UART4(void);
 
-////void UART5_Init(uint32_t Baud);
-////void IntHandler_UART5();
+void UART5_Init(uint32_t Baud);
+void IntHandler_UART5(void);
 
-//void UART7_Init(uint32_t Baud);
-//void IntHandler_UART7();
+void UART7_Init(uint32_t Baud);
+void IntHandler_UART7(void);
 
 #endif /* _UART_CONFIG_H_ */
