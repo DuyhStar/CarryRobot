46ec0e4f488f5a9a32fc9c00d75f8b8d672d3a62
DuYihao <b17090713@njupt.edu.cn>
2019-05-01 19:56:23 +0800

    test tracking function. Finish.

--- User/main.c
+++ User/main.c
@@ -1,11 +1,13 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include "inc/hw_gpio.h"
 #include "driverlib/gpio.h"
 #include "driverlib/pin_map.h"
 #include "inc/hw_memmap.h"
 #include "driverlib/sysctl.h"
 #include "inc/hw_pwm.h"
+#include "driverlib/fpu.h"
 
 #include "delay.h"
 #include "pwm_config.h"
@@ -19,53 +21,39 @@
 #include "qr_code.h"
 #include "timer_config.h"
 
-uint8_t  track_addr;
-uint8_t  forward[8],back[8],left[8],right[8];
+uint8_t  track_addr,forward[8],back[8],left[8],right[8];
 uint16_t servo_angle[] = {90, 90, 90, 90};
 uint8_t  task[3]       = {1,2,3};                       //从二维码中读取的任务信息(1:红. 2:绿. 3:蓝.)
 uint8_t  color[3]      = {1,2,3};                       //物块摆放的颜色顺序
 
+void interrupt_priority_set(void);
 int main()
 {
     SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);//50MHz
-
-    UART0_init(115200);
-    car_ctrl_init();
-    servo_ctrl_init(servo_angle);
-    tracking_init(9600);
-    display_init(9600);
-    wifi_link_init(9600);
-    camera_link_init(9600);
-    qr_code_init(9600);
-
-    timer0_init(100);
+    FPUEnable();
+    FPULazyStackingEnable();
     
-    UARTprintf("All Ready\n");
-    tracking_select(2);
-    car_go_left(50);
-    delay_s(1);
-    car_go_right(50);
-    delay_s(1);
-    car_go_forward(50);
-    delay_s(1);
-    car_go_back(50);
-    delay_s(1);
-    car_stop();
+    UART0_init(115200);
+//    car_ctrl_init();
+//    servo_ctrl_init(servo_angle);
+    tracking_init(9600);
+    
+    printf("All Ready\n");
+    interrupt_priority_set();
+    IntMasterEnable();
     while(1)
     {
-        //car_go_forward(90);
-//        for(uint8_t i = 0; i < 8; i++)
-//        {
-//            UARTprintf("%d",back[i]);
-//        }
-//        UARTprintf("\n");
-//        delay_ms(500);
-//         car_stop();
-//        delay_ms(500);
+        track_inf_print();
+        delay_ms(100);
     }
 }
 
-
+void interrupt_priority_set(void)
+{
+    IntPrioritySet(INT_UART1, 0);
+    IntPrioritySet(INT_TIMER0A, 1);
+    IntPrioritySet(INT_TIMER1A, 2);
+}
 
 
 
