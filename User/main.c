d8457690f7a12421434822e8eab47d95b3cf9771
DuYihao <b17090713@njupt.edu.cn>
2019-05-03 20:07:08 +0800

    test tracking function.

--- User/main.c
+++ User/main.c
@@ -8,7 +8,6 @@
 #include "driverlib/sysctl.h"
 #include "inc/hw_pwm.h"
 #include "driverlib/fpu.h"
-
 #include "delay.h"
 #include "pwm_config.h"
 #include "uart_config.h"
@@ -20,31 +19,38 @@
 #include "wifi.h"
 #include "qr_code.h"
 #include "timer_config.h"
+#include "key.h"
 
-uint8_t  track_addr,forward[8],back[8],left[8],right[8];
-uint16_t servo_angle[] = {90, 90, 90, 90};
-uint8_t  task[3]       = {1,2,3};                       //从二维码中读取的任务信息(1:红. 2:绿. 3:蓝.)
-uint8_t  color[3]      = {1,2,3};                       //物块摆放的颜色顺序
+uint8_t  forward[8],back[8],left[8],right[8];
+uint16_t servo_angle[] = {480, 758, 789, 241};
+uint8_t  task1[3] = {1, 2, 3}, task2[3] = {1, 2, 3};    //从二维码中读取的任务信息(1:红. 2:绿. 3:蓝.)
+uint8_t  color[3] = {1, 2, 3};                          //物块摆放的颜色顺序
 
 void interrupt_priority_set(void);
+
 int main()
 {
-    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);//50MHz
+    SysCtlClockSet(SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);//40MHz
     FPUEnable();
     FPULazyStackingEnable();
     
     UART0_init(115200);
-//    car_ctrl_init();
+    key0_init();
+    car_ctrl_init();
 //    servo_ctrl_init(servo_angle);
-    tracking_init(9600);
+//    delay_s(2);
+    tracking_init();
     
     printf("All Ready\n");
     interrupt_priority_set();
     IntMasterEnable();
+    
+    waitKey();
     while(1)
     {
-        track_inf_print();
-        delay_ms(100);
+        car_trk_line(CAR_FORWARD, 0);
+        //track_inf_print();
+        delay_ms(50);
     }
 }
 
@@ -54,10 +60,3 @@ void interrupt_priority_set(void)
     IntPrioritySet(INT_TIMER0A, 1);
     IntPrioritySet(INT_TIMER1A, 2);
 }
-
-
-
-
-
-
-
