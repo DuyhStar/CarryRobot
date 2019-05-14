b0406935676683b82190f4148dfec8136a20ae3e
DuYihao <b17090713@njupt.edu.cn>
2019-05-14 20:41:03 +0800

    Improve some track  algorithm. Making car move direction can be modifyid randomly.

--- Tracking/tracking.c
+++ Tracking/tracking.c
@@ -3,8 +3,11 @@
 #include <stdio.h>
 #include "timer_config.h"
 #include "car_ctrl.h"
+#include "delay.h"
 
 uint8_t  track_addr = 1;
+
+uint8_t  car_point_dir = CAR_POINT_RIGHT;
 
 void tracking_init(void)
 {
@@ -127,7 +130,7 @@ void car_left_trk_line(void)
         car_rotate(CAR_LEFT_ROTATE);
     }
     else{
-        uint8_t range = 3;
+        uint8_t range = 2;
         if(left_center >= 6+range){
             car_move(CAR_FORWARD);
         }
@@ -153,7 +156,7 @@ void car_right_trk_line(void)
         car_rotate(CAR_RIGHT_ROTATE);
     }
     else{
-        uint8_t range = 3;
+        uint8_t range = 2;
         if(right_center >= 6+range){
             car_move(CAR_BACK);
         }
@@ -196,22 +199,27 @@ int8_t compare_in_range(uint8_t a, uint8_t b)
 void car_go_n_line(uint8_t dir, uint8_t n)
 {
     extern uint8_t forward[8], back[8], left[8], right[8];
-    uint8_t *count_line;
+    uint8_t *count_line, line_center[4] = {12,10,8};
     switch(dir){
         case CAR_FORWARD: count_line = left;    break;
         case CAR_BACK:    count_line = right;   break;
         case CAR_LEFT:    count_line = back;    break;
         case CAR_RIGHT:   count_line = forward; break;
     }
-    uint8_t count = 0, pos = 6;
+    while(count_line[6]){
+        car_move(dir);
+    }
+    uint8_t count = 0, i = 0, center;
     while(1){
         car_trk_line(dir);
-        if(count_line[pos]){
-            pos--;
+        center = find_black_line_center(count_line);
+        if((center == line_center[i]) || (center == line_center[i]-1)){
+            i++;
         }
-        if(pos == 3){
-            pos = 6;
+        if(i == 3){
+            i = 0;
             count++;
+            update_XY(dir);
         }
         if(count == n){
             break;
@@ -219,6 +227,270 @@ void car_go_n_line(uint8_t dir, uint8_t n)
     }
     car_stop();
 }
+
+void update_XY(uint8_t dir)
+{
+    extern int8_t X, Y;
+    switch(car_point_dir){
+        case CAR_POINT_FORWARD:
+            switch(dir) {
+                case CAR_FORWARD: Y++; break;
+                case CAR_BACK:    Y--; break;
+                case CAR_LEFT:    X--; break;
+                case CAR_RIGHT:   X++; break;
+            }
+            break;
+        case CAR_POINT_BACK:
+            switch(dir) {
+                case CAR_FORWARD: Y--; break;
+                case CAR_BACK:    Y++; break;
+                case CAR_LEFT:    X++; break;
+                case CAR_RIGHT:   X--; break;
+            }
+            break;
+        case CAR_POINT_LEFT:
+            switch(dir) {
+                case CAR_FORWARD: X--; break;
+                case CAR_BACK:    X++; break;
+                case CAR_LEFT:    Y--; break;
+                case CAR_RIGHT:   Y++; break;
+            }
+            break;
+        case CAR_POINT_RIGHT:
+            switch(dir) {
+                case CAR_FORWARD: X++; break;
+                case CAR_BACK:    X--; break;
+                case CAR_LEFT:    Y++; break;
+                case CAR_RIGHT:   Y--; break;
+            }
+            break;
+    }
+}
+
+void car_adjust_to_center(void)
+{
+    extern uint16_t w1_speed,w2_speed,w3_speed,w4_speed;
+    extern uint8_t forward[8], back[8], left[8], right[8];
+    uint8_t center_f, center_b, center_l, center_r;
+    
+    w1_speed = w2_speed = w3_speed = w4_speed = 20;
+    //调节100次
+    for(uint8_t i = 0; i < 100; i++){
+        //调整前后方向
+        while(1){
+            center_f = find_black_line_center(forward);
+            center_b = 12 - find_black_line_center(back);
+            
+            if( (center_f == center_b) && (center_f == 6) ){
+                car_stop();
+                break;
+            }
+            else{
+                if(center_f > center_b)
+                    car_rotate(CAR_RIGHT_ROTATE);
+                else if(center_f < center_b)
+                    car_rotate(CAR_LEFT_ROTATE);
+                else{
+                    if(center_f > 6)
+                        car_move(CAR_RIGHT);
+                    else if(center_f < 6)
+                        car_move(CAR_LEFT);
+                    else
+                        ;
+                }
+            }
+        }
+        //调整左右方向
+        while(1){
+            center_l = find_black_line_center(left);
+            center_r = 12 - find_black_line_center(right);
+            
+            if( (center_l == center_r) && (center_l == 6) ){
+                car_stop();
+                break;
+            }
+            else{
+                if(center_l > center_r)
+                    car_rotate(CAR_RIGHT_ROTATE);
+                else if(center_l < center_r)
+                    car_rotate(CAR_LEFT_ROTATE);
+                else{
+                    if(center_l > 6)
+                        car_move(CAR_FORWARD);
+                    else if(center_l < 6)
+                        car_move(CAR_BACK);
+                    else
+                        ;
+                }
+            }
+        }
+    }
+    w1_speed = w2_speed = w3_speed = w4_speed = ALL_WHEEL_SPEED;   
+}
+
+void car_rotate_90_degree(uint8_t dir)
+{
+    extern uint8_t forward[8];
+    
+    car_adjust_to_center();
+    car_rotate(dir);
+    if(dir == CAR_RIGHT_ROTATE){
+        uint8_t i = 6;
+        while(1){
+            if(forward[i] == 1){
+                i--;
+            }
+            if(i == 3){
+                car_stop();
+                break;
+            }
+        }
+    }
+    else{
+        uint8_t i = 0;
+        while(1){
+            if(forward[i] == 1){
+                i++;
+            }
+            if(i == 3){
+                car_stop();
+                break;
+            }
+        }
+    }
+    car_adjust_to_center();
+    
+    //更新车头方向
+    static uint8_t car_point[4] = {CAR_POINT_FORWARD, CAR_POINT_LEFT, CAR_POINT_BACK, CAR_POINT_RIGHT};
+    static int8_t p = 3;
+    if(dir == CAR_LEFT_ROTATE){
+        p++;
+        if(p == 4){
+            p = 0;
+        }
+    }
+    else{
+        p--;
+        if(p == -1){
+            p = 3;
+        }
+    }
+    car_point_dir = car_point[p];
+}
+
+void car_set_point_dir(uint8_t dir)
+{
+    switch(dir){
+        case CAR_POINT_FORWARD:
+            switch(car_point_dir){
+                case CAR_POINT_FORWARD: car_rotate_n_90_degree(CAR_LEFT_ROTATE, 0);  break;
+                case CAR_POINT_BACK:    car_rotate_n_90_degree(CAR_LEFT_ROTATE, 2);  break;
+                case CAR_POINT_LEFT:    car_rotate_n_90_degree(CAR_RIGHT_ROTATE, 1); break;
+                case CAR_POINT_RIGHT:   car_rotate_n_90_degree(CAR_LEFT_ROTATE, 1);  break;
+            }
+            break;
+        case CAR_POINT_BACK:
+            switch(car_point_dir){
+                case CAR_POINT_FORWARD: car_rotate_n_90_degree(CAR_LEFT_ROTATE, 2);  break;
+                case CAR_POINT_BACK:    car_rotate_n_90_degree(CAR_LEFT_ROTATE, 0);  break;
+                case CAR_POINT_LEFT:    car_rotate_n_90_degree(CAR_LEFT_ROTATE, 1);  break;
+                case CAR_POINT_RIGHT:   car_rotate_n_90_degree(CAR_RIGHT_ROTATE, 1); break;
+            }
+            break;
+        case CAR_POINT_LEFT:
+            switch(car_point_dir){
+                case CAR_POINT_FORWARD: car_rotate_n_90_degree(CAR_LEFT_ROTATE, 1);  break;
+                case CAR_POINT_BACK:    car_rotate_n_90_degree(CAR_RIGHT_ROTATE, 1); break;
+                case CAR_POINT_LEFT:    car_rotate_n_90_degree(CAR_RIGHT_ROTATE, 0); break;
+                case CAR_POINT_RIGHT:   car_rotate_n_90_degree(CAR_LEFT_ROTATE, 2);  break;
+            }
+            break;
+        case CAR_POINT_RIGHT:
+            switch(car_point_dir){
+                case CAR_POINT_FORWARD: car_rotate_n_90_degree(CAR_RIGHT_ROTATE, 1); break;
+                case CAR_POINT_BACK:    car_rotate_n_90_degree(CAR_LEFT_ROTATE, 1);  break;
+                case CAR_POINT_LEFT:    car_rotate_n_90_degree(CAR_LEFT_ROTATE, 2);  break;
+                case CAR_POINT_RIGHT:   car_rotate_n_90_degree(CAR_LEFT_ROTATE, 0);  break;
+            }
+            break;
+    }
+}
+
+void car_rotate_n_90_degree(uint8_t dir, uint16_t n)
+{
+    for(uint8_t i = 0; i < n; i++)
+        car_rotate_90_degree(dir);
+}
+
+void car_move_to(uint8_t tar_x, uint8_t tar_y, uint8_t pre)
+{
+    extern int8_t X, Y;
+    int8_t err_x = tar_x - X, err_y = tar_y - Y;
+    
+    if(pre == X_PRE){
+        car_x_move(err_x);
+        car_y_move(err_y);
+    }
+    else if(pre == Y_PRE){
+        car_y_move(err_y);
+        car_x_move(err_x);
+    }
+}
+
+void car_x_move(int8_t err)
+{
+    if(err > 0){
+        switch(car_point_dir){
+            case CAR_POINT_FORWARD: car_go_n_line(CAR_RIGHT, err);    break;
+            case CAR_POINT_BACK:    car_go_n_line(CAR_LEFT, err);     break;
+            case CAR_POINT_LEFT:    car_go_n_line(CAR_BACK, err);     break;
+            case CAR_POINT_RIGHT:   car_go_n_line(CAR_FORWARD, err);  break;
+        }
+    }
+    else if(err < 0){
+        switch(car_point_dir){
+            case CAR_POINT_FORWARD: car_go_n_line(CAR_LEFT, -err);    break;
+            case CAR_POINT_BACK:    car_go_n_line(CAR_RIGHT, -err);   break;
+            case CAR_POINT_LEFT:    car_go_n_line(CAR_FORWARD, -err); break;
+            case CAR_POINT_RIGHT:   car_go_n_line(CAR_BACK, -err);    break;
+        }
+    }
+    else{
+    }
+}
+void car_y_move(int8_t err)
+{
+    if(err > 0){
+        switch(car_point_dir){
+            case CAR_POINT_FORWARD: car_go_n_line(CAR_FORWARD, err);  break;
+            case CAR_POINT_BACK:    car_go_n_line(CAR_BACK, err);     break;
+            case CAR_POINT_LEFT:    car_go_n_line(CAR_RIGHT, err);    break;
+            case CAR_POINT_RIGHT:   car_go_n_line(CAR_LEFT, err);     break;
+        }
+    }
+    else if(err < 0){
+        switch(car_point_dir){
+            case CAR_POINT_FORWARD: car_go_n_line(CAR_BACK, -err);    break;
+            case CAR_POINT_BACK:    car_go_n_line(CAR_FORWARD, -err); break;
+            case CAR_POINT_LEFT:    car_go_n_line(CAR_LEFT, -err);    break;
+            case CAR_POINT_RIGHT:   car_go_n_line(CAR_RIGHT, -err);   break;
+        }
+    }
+    else{
+    }
+}
+
+void car_ready_move(void)
+{
+    extern uint8_t back[8];
+    extern int8_t Y;
+    
+    car_move(CAR_LEFT);
+    while(!back[3])
+        ;
+    Y++;
+    car_go_n_line(CAR_FORWARD, 1);
+}   
 
 uint8_t find_black_line_center(uint8_t* line_inf)
 {
@@ -235,9 +507,18 @@ uint8_t find_black_line_center(uint8_t* line_inf)
             break;
         }
     }
+    //如果没有找到黑线，认为黑线在中间
+    if(begin == 0 && end == 0)
+        return 6;
     
     uint8_t center = begin*2 + end - begin;
     return center;
+}
+
+void car_go_no_line_no_trk(uint8_t dir, uint8_t n)
+{
+    car_move(dir);
+    
 }
 
 
--- Tracking/tracking.h
+++ Tracking/tracking.h
@@ -11,21 +11,39 @@
 #include "inc/hw_types.h"
 #include "inc/hw_gpio.h"
 
-void tracking_init(void);
-void tracking_addr_init(void);
-void tracking_select(uint8_t addr);
+#define X_PRE 0
+#define Y_PRE 1
 
-void car_trk_line(uint8_t dir);
+#define CAR_POINT_FORWARD 0
+#define CAR_POINT_BACK    1
+#define CAR_POINT_LEFT    2
+#define CAR_POINT_RIGHT   3
 
-void car_forward_back_trk_line(uint8_t dir);
-void car_left_trk_line(void);
-void car_right_trk_line(void);
+void    tracking_init(void);
+void    tracking_addr_init(void);
+void    tracking_select(uint8_t addr);
 
-void car_go_n_line(uint8_t dir, uint8_t n);
+void    car_trk_line(uint8_t dir);
+
+void    car_forward_back_trk_line(uint8_t dir);
+void    car_left_trk_line(void);
+void    car_right_trk_line(void);
+
+void    car_ready_move(void);
+void    car_move_to(uint8_t tar_x, uint8_t tar_y, uint8_t pre);
+void    car_x_move(int8_t err);
+void    car_y_move(int8_t err);
+void    car_go_n_line(uint8_t dir, uint8_t n);
+void    update_XY(uint8_t dir);
+
+void    car_adjust_to_center(void);
+void    car_rotate_90_degree(uint8_t dir);
+void    car_rotate_n_90_degree(uint8_t dir, uint16_t n);
+void    car_set_point_dir(uint8_t dir);
 
 uint8_t find_black_line_center(uint8_t* line_inf);
 int8_t  compare_in_range(uint8_t a, uint8_t b);
 
-void track_inf_print(void);
+void    track_inf_print(void);
 
 #endif /* _TRACKING_H_ */
